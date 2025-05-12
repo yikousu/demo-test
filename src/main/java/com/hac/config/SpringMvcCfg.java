c9f2bbec58802745f460f991bc8b31ddf3ac8cc4
yikousu <yikousu369@gmail.com>
2025-05-12 16:52:06 +0800

    feat: WebMvcConfigurer

--- src/main/java/com/hac/config/SpringMvcCfg.java
+++ src/main/java/com/hac/config/SpringMvcCfg.java
@@ -8,6 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author hac
  * @date 2025/5/12 16:40
  */
+
+/**
+ * WebMvcConfigurer 是对 Spring MVC 自动配置的“增强器”
+ * 配置拦截器
+ * 配置跨域
+ * 配置静态资源映射
+ * ...
+ */
 @Configuration
 public class SpringMvcCfg implements WebMvcConfigurer {
     @Override
